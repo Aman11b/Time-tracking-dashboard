3ffe3da2bc99c67ec6e927a3a68ee8ab3c65cd8f
Aman Singh <amansingh1100b@gmail.com>
2025-05-12 19:46:30 +0000

    impleted btn function

--- main.js
+++ main.js
@@ -1,53 +1,76 @@
 // fetching all data
-fetch('data.json')
-.then(response => response.json())
-.then(data =>{
-    console.log('Full Data',data);
-})
-.catch(error => console.error('Error fetching data', error));
+
+// fetch('data.json')
+// .then(response => response.json())
+// .then(data =>{
+//     console.log('Full Data',data);
+// })
+// .catch(error => console.error('Error fetching data', error));
 
 
 // fetech data in format
-fetch('data.json')
-.then(response => response.json())
-.then(data =>{
-    const dailyData = data.map(item => ({title: item.title, daily: item.timeframes.daily}));
-    console.log('Formated data', dailyData)
-})
-.catch(error => console.error('Error fetching data',error))
+
+// fetch('data.json')
+// .then(response => response.json())
+// .then(data =>{
+//     const dailyData = data.map(item => ({title: item.title, daily: item.timeframes.daily}));
+//     console.log('Formated data', dailyData)
+// })
+// .catch(error => console.error('Error fetching data',error))
 
 // fetching only daily data
-fetch('data.json')
-.then(response => response.json())
-.then(data =>{
-    const dailyData =data.map(item => ({
-        title: item.title,
-        current: item.timeframes.daily.current,
-        previous: item.timeframes.daily.previous
-    }));
-    console.log("Daily data:",dailyData);
-}).catch(error => console.log('Error' , error))
+
+// fetch('data.json')
+// .then(response => response.json())
+// .then(data =>{
+//     const dailyData =data.map(item => ({
+//         title: item.title,
+//         current: item.timeframes.daily.current,
+//         previous: item.timeframes.daily.previous
+//     }));
+//     console.log("Daily data:",dailyData);
+// }).catch(error => console.log('Error' , error))
 
 // fetching only weekly data
-fetch('data.json')
-.then(response => response.json())
-.then(data =>{
-    const weeklyData =data.map(item => ({
-        title: item.title,
-        current: item.timeframes.weekly.current,
-        previous: item.timeframes.weekly.previous
-    }));
-    console.log("Weekly data:",weeklyData);
-}).catch(error => console.log('Error' , error))
+
+// fetch('data.json')
+// .then(response => response.json())
+// .then(data =>{
+//     const weeklyData =data.map(item => ({
+//         title: item.title,
+//         current: item.timeframes.weekly.current,
+//         previous: item.timeframes.weekly.previous
+//     }));
+//     console.log("Weekly data:",weeklyData);
+// }).catch(error => console.log('Error' , error))
 
 // fetching only monthly data
-fetch('data.json')
-.then(response => response.json())
-.then(data =>{
-    const monthlyData =data.map(item => ({
-        title: item.title,
-        current: item.timeframes.monthly.current,
-        previous: item.timeframes.monthly.previous
-    }));
-    console.log("monthly data:",monthlyData);
-}).catch(error => console.log('Error' , error))
+
+// fetch('data.json')
+// .then(response => response.json())
+// .then(data =>{
+//     const monthlyData =data.map(item => ({
+//         title: item.title,
+//         current: item.timeframes.monthly.current,
+//         previous: item.timeframes.monthly.previous
+//     }));
+//     console.log("monthly data:",monthlyData);
+// }).catch(error => console.log('Error' , error))
+
+// fetching data as per buttons
+function fetchData(timeframe){
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        const filterData = data.map(item =>({
+            title: item.title,
+            current: item.timeframes[timeframe].current,
+            previous: item.timeframes[timeframe].previous
+        }));
+        console.log(filterData);
+    })
+    .catch(error => console.log('error',error));
+}
+document.getElementById("daily_btn").addEventListener('click',()=>fetchData('daily'));
+document.getElementById("week_btn").addEventListener('click',()=>fetchData('weekly'));
+document.getElementById("Monthly_btn").addEventListener('click',()=>fetchData('monthly'));
