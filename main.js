dd6f997ad7b1915cc5d4d00dbbf49ce401aadf30
Aman11b <amansingh1100b@gmail.com>
2025-05-14 06:06:16 +0530

    data correctly populating ,disgign inconsistancy

--- main.js
+++ main.js
@@ -58,19 +58,82 @@
 // }).catch(error => console.log('Error' , error))
 
 // fetching data as per buttons
-function fetchData(timeframe){
+
+
+// function fetchData(timeframe){
+//     fetch('data.json')
+//     .then(response => response.json())
+//     .then(data => {
+//         const filterData = data.map(item =>({
+//             title: item.title,
+//             current: item.timeframes[timeframe].current,
+//             previous: item.timeframes[timeframe].previous
+//         }));
+//         console.log(filterData);
+//     })
+//     .catch(error => console.log('error',error));
+// }
+// document.getElementById("daily_btn").addEventListener('click',()=>fetchData('daily'));
+// document.getElementById("week_btn").addEventListener('click',()=>fetchData('weekly'));
+// document.getElementById("Monthly_btn").addEventListener('click',()=>fetchData('monthly'));
+
+
+// actual code
+
+const timeframeLabels ={
+    daily: "Yesterday",
+    weekly: "Last week",
+    monthly: "Last Month"
+};
+const taskGrid = document.getElementById('task_grid');
+function fetchData(timeframe ='weekly'){
     fetch('data.json')
     .then(response => response.json())
-    .then(data => {
-        const filterData = data.map(item =>({
-            title: item.title,
-            current: item.timeframes[timeframe].current,
-            previous: item.timeframes[timeframe].previous
-        }));
-        console.log(filterData);
+    .then(data =>{
+        taskGrid.innerHTML ='';
+
+        data.forEach(item=>{
+            const {title, timeframes} = item;
+            const current =timeframes[timeframe].current;
+
+            const previous = timeframes[timeframe].previous;
+
+            const timeframeLabel = timeframeLabels[timeframe];
+
+            const cardHTML =`
+            <div class="task_card | ${title.toLowerCase()}_task ">
+              <div class="card_secondary">
+                <h3>${title}</h3>
+                <svg width="21" height="5" xmlns="http://www.w3.org/2000/svg">
+                  <path
+                    d="M2.5 0a2.5 2.5 0 1 1 0 5
+                  2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5
+                  2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5
+                  2.5 2.5 0 0 1 0-5Z"
+                    fill="#BBC0FF"
+                    fill-rule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div class ='card_content'>
+                <h1>${current} hrs</h1>
+                <p>${timeframeLabel} - ${previous}</p>
+              </div>
+            </div>`;
+            taskGrid.insertAdjacentHTML("beforeend", cardHTML);
+        });
     })
-    .catch(error => console.log('error',error));
+    .catch(error => console.log('Fetch error: ',error))
 }
-document.getElementById("daily_btn").addEventListener('click',()=>fetchData('daily'));
-document.getElementById("week_btn").addEventListener('click',()=>fetchData('weekly'));
-document.getElementById("Monthly_btn").addEventListener('click',()=>fetchData('monthly'));
+
+fetchData('weekly');
+
+document
+  .getElementById("daily_btn")
+  .addEventListener("click", () => fetchData("daily"));
+document
+  .getElementById("week_btn")
+  .addEventListener("click", () => fetchData("weekly"));
+document
+  .getElementById("Monthly_btn")
+  .addEventListener("click", () => fetchData("monthly"));
